27def4713dffe40fe20da666c785af576d561813
Naomi Aro <naomiaro@gmail.com>
2022-04-27 21:47:33 -0700

    formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,35 @@
 <script>
-	// https://www.mathpages.com/home/kmath043.htm
-	import Keyboard from './Keyboard.svelte';
-	import {save} from './save-svg.js';
-	
+  // https://www.mathpages.com/home/kmath043.htm
+  import Keyboard from "./Keyboard.svelte";
+  import { save } from "./save-svg.js";
+
   let octaveWidth = 161;
-	let blackKeyHeight = 80;
-	let whiteKeyHeight = 120;
-	let svg;
-	
-	function reset() {
-		octaveWidth = 161;
-		blackKeyHeight = 80;
-		whiteKeyHeight = 120;
-	}
+  let blackKeyHeight = 80;
+  let whiteKeyHeight = 120;
+  let svg;
+
+  function reset() {
+    octaveWidth = 161;
+    blackKeyHeight = 80;
+    whiteKeyHeight = 120;
+  }
 </script>
 
 <div>
-	<label for="octave_width">Octave Width</label>
-	<input id="octave_width" type=number bind:value={octaveWidth}>
+  <label for="octave_width">Octave Width</label>
+  <input id="octave_width" type="number" bind:value={octaveWidth} />
 </div>
 <div>
-	<label for="octave_width">Black Key Height</label>
-	<input id="octave_width" type=number bind:value={blackKeyHeight}>
+  <label for="octave_width">Black Key Height</label>
+  <input id="octave_width" type="number" bind:value={blackKeyHeight} />
 </div>
 <div>
-	<label for="octave_width">White Key Height</label>
-	<input id="octave_width" type=number bind:value={whiteKeyHeight}>
+  <label for="octave_width">White Key Height</label>
+  <input id="octave_width" type="number" bind:value={whiteKeyHeight} />
 </div>
 <div>
-	<button on:click={ev => save(svg)}>
-		Save
-	</button>
-	<button on:click={reset}>
-		Reset
-	</button>
+  <button on:click={(ev) => save(svg)}> Save </button>
+  <button on:click={reset}> Reset </button>
 </div>
 
 <Keyboard {octaveWidth} {blackKeyHeight} {whiteKeyHeight} bind:svg />
--- src/Keyboard.svelte
+++ src/Keyboard.svelte
@@ -1,17 +1,23 @@
 <script>
-	export let octaveWidth = 161;
-	export let blackKeyHeight = 80;
-	export let whiteKeyHeight = 120;
-	export let svg = undefined;
-	
-	$: W = octaveWidth/7;
-	$: B = octaveWidth/12;
-	$: nar3 = W - B*2/3;
-	$: nar4 = W - B*3/4;
+  export let octaveWidth = 161;
+  export let blackKeyHeight = 80;
+  export let whiteKeyHeight = 120;
+  export let svg = undefined;
+
+  $: W = octaveWidth / 7;
+  $: B = octaveWidth / 12;
+  $: nar3 = W - (B * 2) / 3;
+  $: nar4 = W - (B * 3) / 4;
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="{octaveWidth}" height="{whiteKeyHeight}" bind:this={svg}>
-<!--      
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xml:space="preserve"
+  width={octaveWidth}
+  height={whiteKeyHeight}
+  bind:this={svg}
+>
+  <!--      
 		See http://www.mathpages.com/home/kmath043.htm
      for the math.
      
@@ -21,19 +27,37 @@
      3. The narrow part of white keys C, D and E is W - B*2/3
      4. The narrow part of white keys F, G, A, and B is W - B*3/4 -->
 
-<!--  White keys   -->
-{#each Array(7) as _, row}
-	<rect style="fill:white;stroke:black" x="{W*row}" y="0" width="{W}" height="{whiteKeyHeight}"/>
-{/each}
+  <!--  White keys   -->
+  {#each Array(7) as _, row}
+    <rect
+      style="fill:white;stroke:black"
+      x={W * row}
+      y="0"
+      width={W}
+      height={whiteKeyHeight}
+    />
+  {/each}
 
-<!--  Black keys (overlap with the white keys)  -->
-<!-- 	The narrow part of white keys C, D and E is W - B*2/3 -->
-{#each Array(2) as _, row}
-	<rect style="fill:black;stroke:black" x="{nar3*(row+1)+B*row}" y="0" width="{B}" height="{blackKeyHeight}"/>
-{/each}
-	
-<!-- 	The narrow part of white keys F, G, A, and B is W - B*3/4 -->
-{#each Array(3) as _, row}
-	<rect style="fill:black;stroke:black" x="{W*3+nar4*(row+1)+B*row}" y="0" width="{B}" height="{blackKeyHeight}"/>
-{/each}
+  <!--  Black keys (overlap with the white keys)  -->
+  <!-- 	The narrow part of white keys C, D and E is W - B*2/3 -->
+  {#each Array(2) as _, row}
+    <rect
+      style="fill:black;stroke:black"
+      x={nar3 * (row + 1) + B * row}
+      y="0"
+      width={B}
+      height={blackKeyHeight}
+    />
+  {/each}
+
+  <!-- 	The narrow part of white keys F, G, A, and B is W - B*3/4 -->
+  {#each Array(3) as _, row}
+    <rect
+      style="fill:black;stroke:black"
+      x={W * 3 + nar4 * (row + 1) + B * row}
+      y="0"
+      width={B}
+      height={blackKeyHeight}
+    />
+  {/each}
 </svg>
